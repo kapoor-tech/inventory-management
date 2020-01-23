e4a0714030315008ebc45525755154c2fddf5a2d
Ashish Kapoor <swiftobjc@gmail.com>
2020-01-23 20:45:40 +0530

    fix: asset api

--- routes/assets.js
+++ routes/assets.js
@@ -1,34 +1,32 @@
 const checkAuth = require("../middleware/check-auth");
 
 module.exports = (app, db) => {
-	app.get( "/assets", checkAuth, checkAuth, (_, res) =>
+	// get all assets
+	app.get( "/assets", checkAuth, (_, res) =>
 		db.Assets.findAll().then( (result) => res.json(result) )
 	);
-
+	// get assets by id
 	app.get( "/assets/:id", checkAuth, (req, res) =>
 		db.Assets.findByPk(req.params.id).then( (result) => res.json(result))
 	);
-
-	app.post( "/assets/:id", checkAuth, (req, res) => 
-		db.Shopping_Centers.findByPk(req.params.id)
-		.then(shopping_centers => {
-			shopping_centers.createAsset({
-				name: req.body.name,
-				dimensions: req.body.dimensions,
-				location: req.body.location,
-				status: req.body.status,
-			})
-			.then( (result) => res.json(result) )
-		})
-	);
-
+	// update status by asset id
 	app.put( "/assets/update-status/:id", checkAuth, (req, res) =>
-	db.Assets.update({
-		status: req.body.status,
-	}, {
+		db.Assets.update({
+			status: req.body.status,
+		}, {
 			where: {
 				id: req.params.id
 			}
-		}).then( (result) => res.json(result) )
+		})
+		.then( (result) => res.json(result) )
+	);
+	// get assets by shopping center id
+	app.get("/assets/shopping-center/:id", checkAuth, (req, res) =>
+		db.Assets.findAll({
+			where: {
+				shoppingCenterId: req.params.id 
+			}
+		})
+		.then( (result) => res.json(result) )
 	);
 };
