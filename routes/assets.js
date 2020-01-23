4e338f60c3530e0e4d2fdc26c82c4ac7208517d1
Ashish Kapoor <swiftobjc@gmail.com>
2020-01-23 23:38:35 +0530

    fix: wrongly removed asset create resource

--- routes/assets.js
+++ routes/assets.js
@@ -9,6 +9,19 @@ module.exports = (app, db) => {
 	app.get( "/assets/:id", checkAuth, (req, res) =>
 		db.Assets.findByPk(req.params.id).then( (result) => res.json(result))
 	);
+	// Create assets by shopping center id
+	app.post( "/assets/:id", checkAuth, (req, res) => 
+		db.Shopping_Centers.findByPk(req.params.id)
+		.then(shopping_centers => {
+			shopping_centers.createAsset({
+				name: req.body.name,
+				dimensions: req.body.dimensions,
+				location: req.body.location,
+				status: req.body.status,
+			})
+			.then( (result) => res.json(result) )
+		})
+	);
 	// update status by asset id
 	app.put( "/assets/update-status/:id", checkAuth, (req, res) =>
 		db.Assets.update({
