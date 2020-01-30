de272a3f8d44a5bfebf89d8d1e77d64326e6d0eb
Ashish Kapoor <swiftobjc@gmail.com>
2020-01-30 18:48:31 +0530

    feat: added user login test

---
+++ packages/server/routes/users.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+var host_url = 'http://localhost:3000'
+
+describe('POST /login', function() {
+  it('user login', function(done) {
+    request(host_url)
+      .post('/login')
+      .send({
+        email: 'ashish.kapoor@gmail.com',
+        password: 'abc123'
+      })
+      .set('Accept', 'application/json')
+      .expect((res) => {
+        res.body.users.id = 1;
+        res.body.users.email = 'ashish.kapoor@gmail.com';
+        res.body.users.phone = '9899771880';
+      })
+      .expect(200, done);
+  });
+});
